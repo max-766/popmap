d66a937a5d32f2f1b0a8cd180977b81456ae492a
max-766 <yuji_chiaki@galatea.co.jp>
2025-06-19 22:48:28 +0900

    Update app.js

--- app.js
+++ app.js
@@ -38,6 +38,9 @@ function render(filtered) {
         <p>日本人：${item.japanese.toLocaleString()}人</p>
         <p>外国人：${item.foreign.toLocaleString()}人（${item.rate}%）</p>
         <p>傾向：${item.trend}</p>
+        <div class="text-right mt-4">
+  　　　　　<a href="details.html?city=${encodeURIComponent(item.name)}" class="text-white underline text-sm">詳細を見る</a>
+　　　　</div>
       </div>
     `;
     results.appendChild(div);
